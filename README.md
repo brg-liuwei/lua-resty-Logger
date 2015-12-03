6e8537a9edd5e1865b35ae7e7a3924c4c6bc7462
brg-liuwei <stupidlw@126.com>
2015-12-03 23:51:27 +0800

    v0.02

--- README.md
+++ README.md
@@ -1,10 +1,96 @@
-How to use?
+Name
+====
 
-local logger = require("resty.logger");
+lua-resty-Logger - A local logger for ngx_lua
 
-logger.debug("i am debug");  --for debug
+Table of Contents
+=================
 
-logger.info("i am info");    --for info
+* [Name](#name)
+* [Status](#status)
+* [Description](#description)
+* [Synopsis](#synopsis)
+* [Methods](#methods)
+    * [open](#open)
+    * [close](#close)
+    * [get_level](#get_level)
+    * [set_level](#set_level)
+    * [debug](#debug)
+    * [info](#info)
+    * [error](#error)
+* [Installation](#installation)
+* [TODO](#todo)
+* [Authors](#authors)
+* [Copyrightand License](#copyright-and-license)
 
-logger.error("i am error");  --for error
+Status
+======
+
+This library is still experimental and under early development
+
+Description
+===========
+
+This lua library is a local logging module for ngx_lua,
+
+http://wiki.nginx.org/HttpLuaModule
+
+This is aimed to enhance nginx log. As known to all, ngx.log which supported by ngx_lua prints logs in nginx error_log, however, many users of [openresty](http://openresty.org) as me have a demand for recording bussiness log into a specified file while recording system log into nginx error_log. This module can meed this demand.
+
+This Lua library does NOT take advantage of ngx_lua's coroutine, which is not 100% nonblocking, BUT still effect, for reason that appending-write of os is very fast. 
+
+Synopsis
+========
+
+```lua
+    
+    lua_package_path "/path/to/lua-resty-Logger/lib/?.lua;;";
+    
+    server {
+        location /entry1 {
+            content_by_lua '
+                local logger = require "resty.logger"
+                local buss1 = logger:open("buss1", "/var/log/business1.log")
+                buss1:set_level(logger.INFO)
+                buss1:info("hello world")
+            ';
+        }
+        
+        location /entry2 {
+            log_by_lua '
+                local logger = require "resty.logger"
+                local buss2 = logger:open("buss2", "/var/log/business2.log")
+                buss2:set_level(logger.ERROR)
+                buss2:debug("every thing is ok")
+            ';
+        }
+    }
+```
+
+[Back to TOC](#table-of-contents)
+
+Methods
+=======
+
+open
+----
+
+close
+-----
+
+get_level
+---------
+
+set_level
+---------
+
+debug
+-----
+
+info
+----
+
+error
+-----
+
 
--- lib/resty/logger.lua
+++ lib/resty/logger.lua
@@ -1,77 +1,89 @@
---module("resty.logger", package.seeall)
-
-local bit = require "bit"
+local string = require "string"
+local io = require "io"
 local ffi = require "ffi"
-local ffi_new = ffi.new
-local ffi_str = ffi.string
-local C = ffi.C;
-local bor = bit.bor;
-local ngx_today = ngx.today;
+local C = ffi.C
+local bit = require "bit"
+local bor = bit.bor
 local ngx_localtime = ngx.localtime
-local setmetatable = setmetatable 
-local _error = error    
+local setmetatable = setmetatable
+local _error = error
 
-module(...)
+-- table.new(narr, nrec)
+local succ, new_tab = pcall(require, "table.new")
+if not succ then
+    new_tab = function() return {} end
+end
 
-_VERSION = '0.01'
+local _M = new_tab(0, 5)
 
-local mt = { __index = _M } 
+_M._VERSION = '0.02'
 
 ffi.cdef[[
-int write(int fd, const char *buf, int nbyte);
-int open(const char *path, int access, int mode);
-int close(int fd);
+    int write(int fd, const char *buf, int nbyte);
+    int open(const char *path, int access, int mode);
+    int close(int fd);
 ]]
 
-local O_RDWR   = 0X0002; 
-local O_CREAT  = 0x0040;
-local O_APPEND = 0x0400;
-local S_IRWXU  = 0x01C0;
-local S_IRGRP  = 0x0020;
-local S_IROTH  = 0x0004;
+_M.O_RDONLY = 0
+_M.O_WRONLY = 0
+_M.O_RDWR = 0
+_M.O_CREAT = 0
+_M.O_APPEND = 0
+
+_M.S_IRWXU = 0x01c0
+_M.S_IRGRP = 0x0020
+_M.S_IROTH = 0x0004
+
+local system = string.match(io.popen("uname -a", "r"):read(), "%w+")
+if system == "Darwin" then
+    _M.O_RDONLY = 0x0000
+    _M.O_WRONLY = 0x0001
+    _M.O_RDWR   = 0x0002
+    _M.O_CREAT  = 0x0200
+    _M.O_APPEND = 0x0008
+else -- Linux or other
+    _M.O_RDONLY = 0x0000
+    _M.O_WRONLY = 0x0001
+    _M.O_RDWR   = 0x0002 
+    _M.O_CREAT  = 0x0040
+    _M.O_APPEND = 0x0400
+end
 
 -- log level
-local LVL_DEBUG = 1;
-local LVL_INFO  = 2;
-local LVL_ERROR = 3;
-local LVL_NONE  = 999;
+_M.LVL_DEBUG = 1
+_M.LVL_INFO  = 2
+_M.LVL_ERROR = 3
+_M.LVL_NONE  = 999
 
-local today = ngx_today();
+_M.logger_level = _M.LVL_INFO
+_M.logger_file = "/tmp/some.log"
+_M.logger_fd = C.open(_M.logger_file,
+    bor(_M.O_WRONLY, _M.O_CREAT, _M.O_APPEND),
+    bor(_M.S_IRWXU, _M.S_IRGRP, _M.S_IROTH))
 
-local logger_level = LVL_INFO;
-local logger_file = "/tmp/lomemo_custom.log.";
-local logger_fd = C.open(logger_file..today, bor(O_RDWR, O_CREAT, O_APPEND), bor(S_IRWXU, S_IRGRP, S_IROTH));
-
-function shift_file()
-	if ngx_today() ~= today then
-		C.close(logger_fd);
-		today = ngx_today();
-		logger_fd = C.open(logger_file..today, bor(O_RDWR, O_CREAT, O_APPEND), bor(S_IRWXU, S_IRGRP, S_IROTH));
-	end
+if _M.logger_fd == -1 then
+    error("open log file " .. _M.logger_file .. " failed, errno: " .. tostring(ffi.errno()))
 end
 
-function debug(msg)
-		if logger_level > LVL_DEBUG then return end;
-		shift_file();
+function _M.debug(self, msg)
+		if self.logger_level > self.LVL_DEBUG then return end
 
-		local c = ngx_localtime() .. "|" .."D" .. "|" .. msg .. "\n";
-		C.write(logger_fd, c, #c);
+		local c = ngx_localtime() .. " [DEBUG] " .. msg .. "\n"
+		C.write(self.logger_fd, c, #c)
 end
 
-function info(msg)
-		if logger_level > LVL_INFO then return end;
-		shift_file();
+function _M.info(self, msg)
+		if self.logger_level > self.LVL_INFO then return end
 		
-		local c = ngx_localtime() .. "|" .."I" .. "|" .. msg .. "\n";
-		C.write(logger_fd, c, #c);
+		local c = ngx_localtime() .. " [INFO] " .. msg .. "\n"
+		C.write(self.logger_fd, c, #c)
 end
 
-function error(msg)
-		if logger_level > LVL_ERROR then return end;
-		shift_file();
+function _M.error(self, msg)
+		if self.logger_level > self.LVL_ERROR then return end
 
-		local c = ngx_localtime() .. "|" .."E" .. "|" .. msg .. "\n";
-		C.write(logger_fd, c, #c);
+		local c = ngx_localtime() .. " [ERROR] " .. msg .. "\n"
+		C.write(self.logger_fd, c, #c)
 end
 
 local class_mt = {
@@ -82,3 +94,5 @@ local class_mt = {
 }
 
 setmetatable(_M, class_mt)
+
+return _M
